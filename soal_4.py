d516efef421718e67ec650395c232555f69429e5
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2024-05-07 17:09:46 +0700

    Update soal_4.py

--- soal_4.py
+++ soal_4.py
@@ -13,12 +13,15 @@ def kategori_bmi(bmi):
         return "Obesitas"
 
 def hitung():
-    berat_badan = float(input("Masukkan Berat Badan (Kg) : "))
-    tinggi_badan = float(input("Masukkan Tinggi Badan (M) : "))
-    print('Berat Badan :',berat_badan)
-    print('Tinggi Badan :',tinggi_badan)
+    print("=="*22)
+    berat_badan = float(input("Masukkan Berat Badan (Kg)\t: "))
+    tinggi_badan = float(input("Masukkan Tinggi Badan (M)\t: "))
+    print("=="*22)
+    print('Berat Badan\t\t:',berat_badan)
+    print('Tinggi Badan\t\t:',tinggi_badan)
     bmi = rumus_bmi(berat_badan, tinggi_badan)
-    print("Nilai  BMI Anda :", bmi)
+    print("Nilai  BMI Anda\t\t:", bmi)
     kategori = kategori_bmi(bmi)
-    print("Kategori BMI :", kategori)
+    print("Kategori BMI \t\t:", kategori)
+    print("=="*22)
 hitung()
